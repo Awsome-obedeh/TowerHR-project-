8076ad31fed1a5f5c04509a910c8aa07232fa230
Mona Lisa <124694351+Awsome-obedeh@users.noreply.github.com>
2023-09-15 01:25:44 +0100

    added topnavbar

--- towerhr/src/App.jsx
+++ towerhr/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import SideNav from './components/mainNavigation/sideNav'
+import SideNav from './components/mainNavigation/sideNav';
+import Topnav from './components/mainNavigation/topnav';
 import './App.css'
 
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    {/* <Topnav/> */}
      <SideNav/>
     </>
   )
